9bb5bfae4c8541927513b7fc7729c21b7df9631e
Adam Tokarski <a.tokarski126@gmail.com>
2024-01-04 17:12:09 +0100

    Initial Commit

---
+++ BookNotes/BookNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookNotesApp.swift
+//  BookNotes
+//
+//  Created by Adam Tokarski on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
