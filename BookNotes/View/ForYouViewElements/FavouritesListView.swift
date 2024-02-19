72d11f8e3bb1844bc348d2abca62223165648982
Adam Tokarski <a.tokarski126@gmail.com>
2024-02-19 15:44:02 +0100

    Add SuggestionsListView
    Add SuggestionsListView for suggesting books similar to favourites.
    Move HorizontalScrollViewCell to separate file.

--- BookNotes/View/ForYouViewElements/FavouritesListView.swift
+++ BookNotes/View/ForYouViewElements/FavouritesListView.swift
@@ -8,39 +8,6 @@
 import SwiftData
 import SwiftUI
 
-fileprivate struct HorizontalScrollViewCell: View {
-	let book: Book
-	
-	var body: some View {
-		ZStack {
-			Color(.secondarySystemBackground)
-			
-			VStack {
-				if let data = book.imageData {
-					Image(uiImage: UIImage(data: data)!)
-						.scaledToFit()
-						.frame(width: 100, height: 150)
-				} else {
-					Spacer()
-					
-					Image(systemName: "text.book.closed")
-						.font(.largeTitle)
-				}
-				
-				Spacer()
-				
-				Text(book.title)
-					.font(.headline)
-					.fontDesign(.serif)
-					.lineLimit(1)
-			}
-			.padding()
-		}
-		.frame(width: 150, height: 250)
-		.clipShape(.rect(cornerRadius: 10))
-	}
-}
-
 struct FavouritesListView: View {
 	@EnvironmentObject var favourites: Favourites
 	@Query var books: [Book]
---
+++ BookNotes/View/ForYouViewElements/SuggestionsListView.swift
@@ -0,0 +1,94 @@
+//
+//  SuggestionsList.swift
+//  BookNotes
+//
+//  Created by Adam Tokarski on 19/02/2024.
+//
+
+import SwiftData
+import SwiftUI
+
+struct SuggestionsListView: View {
+	@EnvironmentObject var favourites: Favourites
+	@Query var books: [Book]
+	@State private var fetchedBooks: [APIBook] = []
+	@State private var gettingResults = false
+	@StateObject var networkMonitor = NetworkMonitor()
+	
+	var filteredResults: [APIBook] {
+		fetchedBooks.filter { book in
+			let b = Book(title: book.title, authors: book.authors)
+			
+			return !books.contains(b)
+		}
+	}
+	
+	var body: some View {
+		Group {
+			if fetchedBooks.isEmpty && favourites.count == 0 {
+				EmptyView()
+			} else {
+				VStack(alignment: .leading) {
+					Divider()
+					
+					Text("Based on your favourites")
+						.font(.title2)
+						.bold()
+						.padding(.leading, 5)
+						.padding(.bottom, 5)
+					
+					ScrollView(.horizontal) {
+						HStack {
+							ForEach(filteredResults, id: \.self) { book in
+								NavigationLink {
+									DetailViewAPI(book: book, bookInCollection: false)
+								} label: {
+									HorizontalScrollViewCell(book)
+										.padding(.horizontal, 5)
+										.tint(.primary)
+								}
+							}
+						}
+						.scrollTargetLayout()
+					}
+					.scrollTargetBehavior(.viewAligned)
+					.scrollIndicators(.hidden)
+				}
+			}
+		}
+		.onChange(of: networkMonitor.isConnected) { _, newValue in
+			if newValue {
+				performRequest()
+			}
+		}
+		.onChange(of: favourites.count) { _, newValue in
+			if newValue == 0 {
+				fetchedBooks = []
+			}
+		}
+		.onAppear(perform: performRequest)
+	}
+	
+	private func performRequest() {
+		guard networkMonitor.isConnected else { return }
+		
+		for (i, title) in favourites.favouriteAuthors.enumerated() {
+			Task {
+				let results = await APIConnector.getApiResults(for: title, .author)
+				Task { @MainActor in
+					withAnimation {
+						if i == 0 {
+							fetchedBooks = results
+						} else {
+							fetchedBooks.append(contentsOf: results)
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#Preview {
+	SuggestionsListView()
+}
