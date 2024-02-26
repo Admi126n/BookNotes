b0e69d2a08216938e2b9aeb79a167e61b8fb871b
Adam Tokarski <a.tokarski126@gmail.com>
2024-02-26 20:00:29 +0100

    Add editing cover to EditBookView

--- BookNotes/View/EditBookView.swift
+++ BookNotes/View/EditBookView.swift
@@ -7,11 +7,15 @@
 
 import SwiftData
 import SwiftUI
+import PhotosUI
 
 struct EditBookView: View {
 	@Binding var book: Book
 	@Environment(\.dismiss) var dismiss
 	@State private var authors: String
+	
+	@State private var pickerItem: PhotosPickerItem?
+	@State private var imageData: Data?
 	
 	var body: some View {
 		NavigationStack {
@@ -24,7 +28,32 @@ struct EditBookView: View {
 					TextField("Author", text: $authors)
 				}
 				
-				// Cover
+				Section("Cover") {
+					PhotosPicker(selection: $pickerItem,
+								 matching: .any(of: [.images, .screenshots])) {
+						if let imageData = book.imageData {
+							HStack(spacing: 10) {
+								Image(uiImage: UIImage(data: imageData)!)
+									.resizable()
+									.scaledToFit()
+									.frame(width: 100)
+									.clipShape(.rect(cornerRadius: 5))
+								
+								Label("Edit cover", systemImage: "photo.on.rectangle")
+							}
+						} else {
+							Label("Add cover", systemImage: "photo.badge.plus")
+						}
+					}
+					.swipeActions(edge: .trailing, allowsFullSwipe: true) {
+						if book.imageData != nil {
+							DeleteButton {
+								book.setImageData(nil)
+								pickerItem = nil
+							}
+						}
+					}
+				}
 				
 				// Categories
 				
@@ -41,6 +70,12 @@ struct EditBookView: View {
 			}
 			.onChange(of: authors) {
 				book.setAuthors(authors)
+			}
+			.onChange(of: pickerItem) {
+				Task {
+					imageData = try await pickerItem?.loadTransferable(type: Data.self)
+					book.setImageData(imageData)
+				}
 			}
 		}
 	}
